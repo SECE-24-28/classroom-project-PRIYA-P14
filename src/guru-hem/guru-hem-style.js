d22b41f08a86913ce3c45933c3fc81127cbd0af8
PRIYA-P14 <priya.p2024cse@sece.ac.in>
2025-12-08 14:59:21 +0530

    Add files via upload
    Style is applying

---
+++ src/guru-hem/guru-hem-style.js
@@ -0,0 +1,214 @@
+import styled from "styled-components";
+
+export const GuruHemStyle = styled.div`
+.unlock {
+    text-align: left;
+    margin-top: 8rem;
+    margin-left: 3rem;
+}
+.unlock h1 {
+    font-size: 60px;
+    font-weight: 600;
+    line-height: 1.2;
+    color: rgb(38, 38, 38);
+    max-width: 400px;
+    margin-bottom: 0rem;
+}
+.unlock h2 {
+    color: rgb(71, 85, 105);
+    font-size: 18px;
+    font-weight: 400;
+    margin: 1.5rem 0px;
+    max-width: 450px;
+    line-height: 26px;
+    margin-left: 0rem;
+   // margin-bottom: 1rem;
+    font-family: Inter, sans-serif;
+}
+.buttons {
+    display: flex;
+    justify-content: center;
+    margin-top: 2rem;
+    margin-left: 8rem;
+}
+.start {
+margin-left: 80px;
+margin-right: -230px;
+display: flex;
+justify-content: space-between;
+align-items: center;
+margin-bottom: 160px;
+margin-top: -600px;
+}
+.start h2 {
+    background-color: rgba(255, 25, 25, 1);
+    color: white;
+    width: 200px;
+    height: 50px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 7px;
+    font-size: 20px;
+    margin-bottom: 10px;
+    margin-top: 500px;
+}
+.get {
+margin-left: 300px;
+margin-right: 100px;
+display: flex;
+justify-content: right;
+align-items: center;
+margin-bottom: -90px;
+margin-top: -500px;
+}
+.get h2 {
+    background-color: rgba(248, 28, 28, 1);
+    color: white;
+    width: 200px;
+    height: 50px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 7px;
+    font-size: 20px;
+    margin-bottom: 580px;
+    margin-top: -420px;
+    margin-right: -1000px;
+}
+.login {
+margin-left: 600px;
+margin-right: 100px;
+display: flex;
+justify-content: right;
+align-items: center;
+margin-bottom: 10px;
+margin-top: -675px;
+}
+.login h2 {
+    color: red;
+    width: 200px;
+    height: 50px;
+    border-radius: 7px;
+    font-size: 23px;
+    margin-bottom: 600px;
+    margin-top: -90px;
+    margin-right: -10px;
+    margin-left: 10px;
+}
+.agh {
+    color: rgb(252, 41, 71);
+}
+.lms {
+    color: rgb(41, 153, 255);
+}
+.Topbar {
+    display: flex;
+    justify-content: space-between;
+    align-items: left;
+
+}
+.guruhem {
+    font-size: 25px;
+    font-weight: 400;
+    text-align: center;
+    font-family: Inter, sans-serif;
+    margin-bottom: 2rem;
+}
+
+.description {
+    display: flex;
+    justify-content: center;
+    margin-top: 3rem;
+}
+.glance {
+    color: rgb(252, 41, 71);
+    margin-left: 10px;
+}
+.textblock h2 {
+    display: flex;
+    justify-content: left;
+    font-size: 40px;
+    font-weight: 600;
+    line-height: 50px;
+    align-items: right;
+    margin-left:50px;
+
+}
+.textblock h3{
+    display: flex;
+    justify-content: left;
+    font-size: 18px;
+    font-weight: 400;
+    color: rgb(71, 85, 105);
+    line-height: 32px;
+    margin-left:50px;
+}
+
+.highlight {
+    color: rgb(252, 41, 71);
+}
+.Wholediv {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+}
+.subhead1 {
+display: flex;
+justify-content: center;
+margin-bottom: 2rem;
+margin-top: 2rem;
+margin-left: 0rem;
+//border-radius: 15px;
+}
+.subhead2{
+display: flex;
+justify-content: center;
+}
+.colleges {
+background-color: rgb(255, 223, 228);
+width: 450px;
+height: 100px;
+padding: 1rem 1rem ;
+margin-right: 2rem;
+border-radius: 15px;
+font-family: Inter, sans-serif;
+font-weight: 200;
+margin-bottom: 5px;
+color: rgb(51, 65, 85);
+}
+.students {
+background-color: rgb(226, 240, 250);
+width: 450px;
+height: 100px;
+padding: 1rem 1rem ;
+border-radius: 15px;
+font-family: Inter, sans-serif;
+font-weight: 200;
+margin-bottom: 5px;
+color: rgb(51, 65, 85);
+}
+.studymaterials {
+background-color: rgb(222, 255, 236);
+width: 450px;
+height: 100px;
+padding: 1rem 1rem ;
+margin-right: 2rem;
+border-radius: 15px;
+font-family: Inter, sans-serif;
+font-weight: 200;
+margin-bottom: 5px;
+color: rgb(51, 65, 85);
+}
+.trainers {
+background-color: rgb(255, 230, 199);
+width: 450px;
+height: 100px;
+padding: 1rem 1rem ;
+border-radius: 15px;
+font-family: Inter, sans-serif;
+font-weight: 200;
+    margin-bottom: 5px;
+    color: rgb(51, 65, 85);
+}
+`;
